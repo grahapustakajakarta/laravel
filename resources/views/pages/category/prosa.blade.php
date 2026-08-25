f4f1c50304a443c4a05b59a8df31b560f43d0deb
ismiatiandini <ismiatiandini6@gmail.com>
2026-08-25 18:54:16 +0700

    Resize sponsor logos in Prosa header and fix Plan International image file

--- resources/views/pages/category/prosa.blade.php
+++ resources/views/pages/category/prosa.blade.php
@@ -466,10 +466,10 @@
         <div class="prosa-page-header">
             <h1 style="margin-bottom: 0;">Prosa</h1>
             <div class="prosa-sponsors" style="display: flex; align-items: center; gap: 20px;">
-                <img src="{{ asset('img/sponsor/Sarinah png.png') }}" alt="Sarinah" style="height: 55px; object-fit: contain;">
-                <img src="{{ asset('img/sponsor/Kompas.png') }}" alt="Kompas" style="height: 55px; object-fit: contain;">
-                <img src="{{ asset('img/sponsor/images (1).png') }}" alt="Plan International" style="height: 55px; object-fit: contain;">
-                <img src="{{ asset('img/sponsor/Ford-Logo-1536x864.png') }}" alt="Ford" style="height: 55px; object-fit: contain;">
+                <img src="{{ asset('img/sponsor/Sarinah png.png') }}" alt="Sarinah" style="height: 32px; object-fit: contain;">
+                <img src="{{ asset('img/sponsor/Kompas.png') }}" alt="Kompas" style="height: 32px; object-fit: contain;">
+                <img src="{{ asset('img/sponsor/Plan_International_Logo.svg.png') }}" alt="Plan International" style="height: 32px; object-fit: contain;">
+                <img src="{{ asset('img/sponsor/Ford-Logo-1536x864.png') }}" alt="Ford" style="height: 32px; object-fit: contain;">
             </div>
         </div>
 
