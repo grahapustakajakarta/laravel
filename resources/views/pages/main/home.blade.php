ad0dd3248ec59eae125f38e753928bb0abe5e19e
ismiatiandini <ismiatiandini6@gmail.com>
2026-08-27 14:28:46 +0700

    Widen Review Book right column: reduce thumbnail, rebalance col layout

--- resources/views/pages/main/home.blade.php
+++ resources/views/pages/main/home.blade.php
@@ -906,7 +906,7 @@
                 </div>
                 <div class="row">
                     @if($bukuTerbaru->isNotEmpty())
-                    <div class="col-lg-4 col-md-4 review-main-grid">
+                    <div class="col-lg-5 col-md-5 review-main-grid">
                         @foreach($bukuTerbaru as $buku)
                         <div class="buku-terbaru-item" style="flex: 1; min-width: 0; display: flex; flex-direction: column; text-align: left;">
                             <a href="{{ url('/artikel/'.$buku->slug) }}" style="text-decoration: none; display:block; margin-bottom: 16px;">
@@ -922,7 +922,7 @@
                         @endforeach
                     </div>
                     
-                    <div class="col-lg-8 col-md-8 review-side-grid">
+                    <div class="col-lg-7 col-md-7 review-side-grid">
                         @foreach ($bukuLama as $row)
                         <div class="content buku-lama-item" style="display: flex; justify-content: space-between; align-items: flex-start; margin-bottom: 24px; width: 100%;">
                             <div class="desc" style="flex: 1; padding-right: 15px; text-align: left;">
@@ -932,7 +932,7 @@
                                     <p style="font-family: var(--font-sans); font-size: 11.5px; font-weight: 700; color: #b70d0f; margin: 6px 0 0 0; text-transform: uppercase;">By {{ $row->penulis->nama ?? '-' }}</p>
                                 </a>
                             </div>
-                            <img src="{{ asset('img/'.$row->gambar_pertama) }}" style="width:105px; height:130px; object-fit:contain; object-position:center; flex-shrink:0; border-radius: 2px;" alt="">
+                            <img src="{{ asset('img/'.$row->gambar_pertama) }}" style="width:75px; height:95px; object-fit:contain; object-position:center; flex-shrink:0; border-radius: 2px;" alt="">
                         </div>
                         @endforeach
                     </div>
